7602c05266ab0f67acdd5b233fadff777d9ac5e1
madstone290 <madstone290@gmail.com>
2022-06-14 13:40:15 +0900

    Add EFCore 프로젝트

---
+++ EFCore.Core/Data/UserDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EFCore.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Core.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
