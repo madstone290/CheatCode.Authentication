557f58d27394b94b1681bd7630229d2d1f246ce2
madstone290 <madstone290@gmail.com>
2022-06-30 14:35:14 +0900

    Add mudblazor dialog

---
+++ Blazor/MudBlazorApp/Pages/ViewModels/BrandModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MudBlazorApp.Pages.ViewModels
+{
+    public class BrandModel 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Tax { get; set; }
+    }
+}
